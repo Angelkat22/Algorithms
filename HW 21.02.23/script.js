5ba8299734ab6f76ff06ba4ff405fec6a67c1213
Katrina <113929793+Angelkat22@users.noreply.github.com>
2023-02-25 18:35:33 +0200

    Greedy Algorithms

--- HW 21.02.23/script.js
+++ HW 21.02.23/script.js
@@ -10,53 +10,53 @@
 // например с 12 до 14 стоит 2 ед
 
 
-// class  Activity {
-//     name
-//     startTime
-//     endTime
+class  Activity {
+    name
+    startTime
+    endTime
 
-//     constructor(name, startTime, endTime) {
-//         this.name = name
-//         this.startTime = startTime;
-//         this.endTime = endTime;
-//     }
-// }
+    constructor(name, startTime, endTime) {
+        this.name = name
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
 
-// function maximizeNumberOfActivities(activities) {
-//     function maximizeNumberOfActivities(activities) {
-//         // сортируем мероприятия по времени окончания
-//         activities.sort((activity1, activity2) => activity1.endTime - activity2.endTime)
-//         // создаем контейнер, где будем хранить результат
-//         const selectedActivities = []
-//         // мероприятие, которое заканчивается раньше всех a priori попадет в контейнер
-//         const firstActivity = activities[0]
-//         selectedActivities.push(firstActivity)
-//         let lastChosenActivity = firstActivity;
-//         // по порядку проходим по всем мероприятиям
-//         for (let i = 0; i < activities.length; i++) {
-//             let currentActivity = activities[i]
-//             // условие непересечения
-//             if (currentActivity.startTime >= lastChosenActivity.endTime) {
-//                 selectedActivities.push(currentActivity)
-//                 lastChosenActivity = currentActivity
-//             }
-//         }
-//         // returns list of selected activities
-//         return selectedActivities
-//     }
-
-
-// }
-
-// /// Check
-// const activitiesToSchedule = [];
-// activitiesToSchedule.push(new Activity("A", 900,1300))
-// activitiesToSchedule.push(new Activity("B", 1300,1700))
-// activitiesToSchedule.push(new Activity("C", 1100,1400))
-// activitiesToSchedule.push(new Activity("D", 1200,1600))
+function maximizeNumberOfActivities(activities) {
+    function maximizeNumberOfActivities(activities) {
+        // сортируем мероприятия по времени окончания
+        activities.sort((activity1, activity2) => activity1.endTime - activity2.endTime)
+        // создаем контейнер, где будем хранить результат
+        const selectedActivities = []
+        // мероприятие, которое заканчивается раньше всех a priori попадет в контейнер
+        const firstActivity = activities[0]
+        selectedActivities.push(firstActivity)
+        let lastChosenActivity = firstActivity;
+        // по порядку проходим по всем мероприятиям
+        for (let i = 0; i < activities.length; i++) {
+            let currentActivity = activities[i]
+            // условие непересечения
+            if (currentActivity.startTime >= lastChosenActivity.endTime) {
+                selectedActivities.push(currentActivity)
+                lastChosenActivity = currentActivity
+            }
+        }
+        // returns list of selected activities
+        return selectedActivities
+    }
 
 
-// const selectedActivities = maximizeNumberOfActivities(activitiesToSchedule)
+}
 
-// console.log(selectedActivities)
+/// Check
+const activitiesToSchedule = [];
+activitiesToSchedule.push(new Activity("A", 900,1300))
+activitiesToSchedule.push(new Activity("B", 1300,1700))
+activitiesToSchedule.push(new Activity("C", 1100,1400))
+activitiesToSchedule.push(new Activity("D", 1200,1600))
+
+
+const selectedActivities = maximizeNumberOfActivities(activitiesToSchedule)
+
+console.log(selectedActivities)
 
